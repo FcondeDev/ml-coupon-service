e95bdd4dddaccd07785a8c4f59e9b7955860b9ff
a <a>
2020-07-19 17:23:15 -0500

    initial commit

---
+++ src/main/java/com/ml/coupon/MlCouponServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ml.coupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MlCouponServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MlCouponServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ml/coupon/MlCouponServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ml.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MlCouponServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
