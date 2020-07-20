ad84b4b5e69302bcb6c7f2fa68b1709001c314a4
a <a>
2020-07-20 12:25:32 -0500

    changing error message

--- src/main/java/com/ml/coupon/error/ErrorEnum.java
+++ src/main/java/com/ml/coupon/error/ErrorEnum.java
@@ -6,7 +6,7 @@ public enum ErrorEnum {
 			"Ninguno de los items se puede adquirir con el cupon suministrado"),
 	DEFAULT_EXCEPTION(3, "Error", "Se presento un error, intentelo mas tarde"),
 	FEIGN_EXCEPTION(4, "Error mercado libre API", "Hubo un error contactando el api de mercado libre"),
-	ITEM_NOT_FOUND_ML_API(4, "Error mercado libre API", "No fue posible encontrar informacion en el API de mercado libre para alguno de los items asociados");
+	ITEM_NOT_FOUND_ML_API(4, "Error mercado libre API", "No fue posible encontrar informacion en el API de mercado libre para alguno de los items recibidos");
 
 	public final int code;
 	public final String title;
